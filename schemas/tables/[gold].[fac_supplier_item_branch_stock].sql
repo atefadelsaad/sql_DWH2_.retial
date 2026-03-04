418b1cc8a5b9c11c4f120d81d23e7448b1307737
Atef Adel <atefadel1996123@gmail.com>
2026-03-04 19:31:36 +0200

    Create [gold].[fac_supplier_item_branch_stock].sql

---
+++ schemas/tables/[gold].[fac_supplier_item_branch_stock].sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS gold.fac_supplier_item_branch_stock
+CREATE TABLE gold.fac_supplier_item_branch_stock(
+	supplier_no int  NOT NULL,
+	branch int  NOT NULL,
+	order_date  date  NOT NULL,
+	doctype  int  NOT NULL,
+	iteamean  char(13)  NOT NULL,
+	stock_total_value decimal(18, 5) NOT NULL,
+	total_actual_qty decimal(18, 2) NOT NULL,
+	total_free_qty decimal(18, 2) NOT NULL
+PRIMARY KEY CLUSTERED 
+(
+	[supplier_no] ASC,
+	[branch] ASC,
+	[order_date] DESC,
+	[doctype] ASC,
+	[iteamean] ASC
+))
