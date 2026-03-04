3a19c209d8b6150da9ae9ba71ac7ddfa032ca9f1
Atef Adel <atefadel1996123@gmail.com>
2026-03-04 19:39:55 +0200

    Update and rename [gold].[fac_supplier_item_branch_stock].sql to [gold].[fac_supplier_item_branch_stock_purchases].sql

--- schemas/tables/[gold].[fac_supplier_item_branch_stock_purchases].sql
+++ schemas/tables/[gold].[fac_supplier_item_branch_stock_purchases].sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS gold.fac_supplier_item_branch_stock
-CREATE TABLE gold.fac_supplier_item_branch_stock(
+	 DROP TABLE IF EXISTS gold.fac_supplier_item_branch_stock_purchases
+CREATE TABLE gold.fac_supplier_item_branch_stock_purchases(
 	supplier_no int  NOT NULL,
 	branch int  NOT NULL,
 	order_date  date  NOT NULL,
@@ -7,7 +7,8 @@ CREATE TABLE gold.fac_supplier_item_branch_stock(
 	iteamean  char(13)  NOT NULL,
 	stock_total_value decimal(18, 5) NOT NULL,
 	total_actual_qty decimal(18, 2) NOT NULL,
-	total_free_qty decimal(18, 2) NOT NULL
+	total_free_qty decimal(18, 2) NOT NULL,
+	last_update date NOT NULL
 PRIMARY KEY CLUSTERED 
 (
 	[supplier_no] ASC,
